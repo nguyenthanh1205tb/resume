983188d49d7f7d2f65193512bb83d4574e8a7a7b
nguyenthanh1205tb <nguyenthanh1205tb@gmail.com>
2023-03-02 11:40:29 +0700

    remove analyze

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
   style: {},
@@ -16,17 +14,6 @@ module.exports = {
             comments: false,
           },
         },
-      }),
-    ],
-  },
-  webpack: {
-    plugins: [
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'server',
-        analyzerHost: '127.0.0.1',
-        analyzerPort: 8888,
-        openAnalyzer: false,
-        reportFilename: path.resolve(__dirname, `analyzer/index.html`),
       }),
     ],
   },
