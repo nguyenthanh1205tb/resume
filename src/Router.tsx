821851f8202b9fa83956ca345e91791da617859c
thanh nguyen <43309718+nguyenthanh1205tb@users.noreply.github.com>
2023-08-17 17:41:48 +0700

    Dev (#4)
    * remove readme
    
    * update

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,6 @@ import PublicAuth from 'src/components/Auth/PublicAuth'
 import About from 'src/pages/About'
 import NotFound from 'src/pages/Exception/NotFound'
 import Portfolio from 'src/pages/Portfolio'
-import Resume from 'src/pages/Resume'
 
 function Router() {
   return (
@@ -14,7 +13,6 @@ function Router() {
       <Route path="/" element={<Navigate to="/about" />} />
       <Route element={<PublicAuth />}>
         <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
         <Route path="/portfolio" element={<Portfolio />} />
       </Route>
       {/* <Route path="/about" element={<About />} /> */}
